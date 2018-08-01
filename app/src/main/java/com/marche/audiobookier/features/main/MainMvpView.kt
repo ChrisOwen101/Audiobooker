7157c34fcba43550be32c0d20a0fd6cac7839a30
Chris Owen <chris@thecodethatchriswrote.com>
2018-08-01 20:18:16 +0100

    Remove pokemon stuff

--- app/src/main/java/com/marche/audiobookier/features/main/MainMvpView.kt
+++ app/src/main/java/com/marche/audiobookier/features/main/MainMvpView.kt
@@ -4,10 +4,4 @@ import com.marche.audiobookier.features.base.MvpView
 
 interface MainMvpView : MvpView {
 
-    fun showPokemon(pokemon: List<String>)
-
-    fun showProgress(show: Boolean)
-
-    fun showError(error: Throwable)
-
 }
--- app/src/main/java/com/marche/audiobookier/features/main/MainPresenter.kt
+++ app/src/main/java/com/marche/audiobookier/features/main/MainPresenter.kt
@@ -3,28 +3,14 @@ package com.marche.audiobookier.features.main
 import com.marche.audiobookier.data.DataManager
 import com.marche.audiobookier.features.base.BasePresenter
 import com.marche.audiobookier.injection.ConfigPersistent
-import com.marche.audiobookier.util.rx.scheduler.SchedulerUtils
 import javax.inject.Inject
 
 @ConfigPersistent
 class MainPresenter @Inject
 constructor(private val dataManager: DataManager) : BasePresenter<MainMvpView>() {
 
-    fun getPokemon(limit: Int) {
+    fun getAudiobooks(limit: Int) {
         checkViewAttached()
-        mvpView?.showProgress(true)
-        dataManager.getPokemonList(limit)
-                .compose(SchedulerUtils.ioToMain<List<String>>())
-                .subscribe({ pokemons ->
-                    mvpView?.apply {
-                        showProgress(false)
-                        showPokemon(pokemons)
-                    }
-                }) { throwable ->
-                    mvpView?.apply {
-                        showProgress(false)
-                        showError(throwable)
-                    }
-                }
+
     }
 }
